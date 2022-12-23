8404b06f64c2fd0535cf5039ad83ce490b067495
dejw7n <dejw7n@gmail.com>
2022-12-23 08:27:46 +0100

    Update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,24 @@
+import { AdminComponent } from "./modules/admin/admin.component";
 import { NgModule } from "@angular/core";
 import { NoPreloading, RouterModule, Routes } from "@angular/router";
 import { PublicComponent } from "./modules/public/public.component";
+import { AuthComponent } from "./modules/auth/auth.component";
 
 const routes: Routes = [
 	{
 		path: "",
 		component: PublicComponent,
 		loadChildren: () => import("./modules/public/public.module").then((m) => m.PublicModule),
+	},
+	{
+		path: "auth",
+		component: AuthComponent,
+		loadChildren: () => import("./modules/auth/auth.module").then((m) => m.AuthModule),
+	},
+	{
+		path: "admin",
+		component: AdminComponent,
+		loadChildren: () => import("./modules/admin/admin.module").then((m) => m.AdminModule),
 	},
 ];
 
