0100c23a5e54493e62829ed0dfef3b7174f8ba69
dejw7n <dejw7n@gmail.com>
2022-12-23 08:08:49 +0100

    Add public component

--- src/app/modules/public/public.module.ts
+++ src/app/modules/public/public.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PublicComponent } from './public/public.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PublicComponent
+  ],
   imports: [
     CommonModule
   ]
