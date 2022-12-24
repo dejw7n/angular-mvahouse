5fea5f1b9d9605fb1e066d376fef096e282c22df
dejw7n <dejw7n@gmail.com>
2022-12-24 17:45:49 +0100

    Add website layout

--- src/app/modules/public/public.module.ts
+++ src/app/modules/public/public.module.ts
@@ -1,11 +1,15 @@
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { SharedModule } from "./../../shared/shared.module";
 import { PublicRoutingModule } from "./public-routing.module";
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { PublicComponent } from "./public.component";
 import { HomeComponent } from "./pages/home/home.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { SearchFormComponent } from "./components/search-form/search-form.component";
 
 @NgModule({
-	declarations: [PublicComponent, HomeComponent],
-	imports: [PublicRoutingModule, CommonModule],
+	declarations: [PublicComponent, HomeComponent, HeaderComponent, SearchFormComponent],
+	imports: [PublicRoutingModule, SharedModule, FontAwesomeModule, CommonModule],
 })
 export class PublicModule {}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const themeColors = {
+	primary: "#9f7627",
+	secondary: "#35459E",
+	third: "#FF3541",
+};
+
 module.exports = {
 	content: ["./src/**/*.{html,ts}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: themeColors["primary"],
+				secondary: themeColors["secondary"],
+				third: themeColors["third"],
+			},
+		},
 	},
 	plugins: [require("@tailwindcss/typography")],
 };
