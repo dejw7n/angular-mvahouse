a0021aec3acac61ccea7908c473f185a99b6867c
dejw7n <dejw7n@gmail.com>
2023-01-06 14:44:47 +0100

    Add alt to images

--- src/app/modules/public/pages/home/home.component.ts
+++ src/app/modules/public/pages/home/home.component.ts
@@ -9,24 +9,31 @@ export class HomeComponent {
 	introduceCarouselSlides = [
 		{
 			imgSrc: "assets/images/mvahouse/interior/1.jpg",
+			imgAlt: "MVA House Interior",
 		},
 		{
 			imgSrc: "assets/images/mvahouse/interior/2.jpg",
+			imgAlt: "MVA House Interior",
 		},
 		{
 			imgSrc: "assets/images/mvahouse/interior/3.jpg",
+			imgAlt: "MVA House Interior",
 		},
 		{
 			imgSrc: "assets/images/mvahouse/interior/4.jpg",
+			imgAlt: "MVA House Interior",
 		},
 		{
 			imgSrc: "assets/images/mvahouse/interior/5.jpg",
+			imgAlt: "MVA House Interior",
 		},
 		{
 			imgSrc: "assets/images/mvahouse/interior/6.jpg",
+			imgAlt: "MVA House Interior",
 		},
 		{
 			imgSrc: "assets/images/mvahouse/interior/7.jpg",
+			imgAlt: "MVA House Interior",
 		},
 	];
 }
