f018b21eec9605e980e851a95f78f7f95b1e4e9d
dejw7n <dejw7n@gmail.com>
2023-01-15 20:43:55 +0100

    Add slider module

--- src/app/modules/public/public.module.ts
+++ src/app/modules/public/public.module.ts
@@ -8,15 +8,17 @@ import { HomeComponent } from "./pages/home/home.component";
 import { HeaderComponent } from "./components/header/header.component";
 import { SearchFormComponent } from "./components/search-form/search-form.component";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
-import { HeroBannerComponent } from "./components/hero-banner/hero-banner.component";
 import { ToggleButtonModule } from "primeng/togglebutton";
 import { OverlayModule } from "primeng/overlay";
 import { NzButtonModule } from "ng-zorro-antd/button";
 import { NzCarouselModule } from "ng-zorro-antd/carousel";
 import { FormsModule } from "@angular/forms";
+import { SearchResultsComponent } from "./pages/search-results/search-results.component";
+import { SliderModule } from "primeng/slider";
+import { RoomComponent } from "./pages/room/room.component";
 
 @NgModule({
-	declarations: [PublicComponent, HomeComponent, HeaderComponent, SearchFormComponent, HeroBannerComponent],
-	imports: [PublicRoutingModule, CoreModule, SharedModule, FormsModule, NzCarouselModule, OverlayModule, ToggleButtonModule, NzButtonModule, FontAwesomeModule, CommonModule],
+	declarations: [PublicComponent, HomeComponent, HeaderComponent, SearchFormComponent, SearchResultsComponent, RoomComponent],
+	imports: [PublicRoutingModule, CoreModule, SharedModule, SliderModule, FormsModule, NzCarouselModule, OverlayModule, ToggleButtonModule, NzButtonModule, FontAwesomeModule, CommonModule],
 })
 export class PublicModule {}
