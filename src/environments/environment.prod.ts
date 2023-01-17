477929f4b6e390335ea76218d1e73aaf325b7e0b
dejw7n <dejw7n@gmail.com>
2023-01-17 12:00:34 +0100

    Add environments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+	production: true,
+	apiHost: "https://mvahousecapital.pages.dev/",
+	apiKey: "prodKey",
+};
---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,5 @@
+export const environment = {
+	production: true,
+	apiHost: "http://localhost:4200/",
+	apiKey: "devKey",
+};
