de86a827c36457b66adda97b2ce94702590425af
dejw7n <dejw7n@gmail.com>
2023-01-18 11:52:20 +0100

    Update room component

--- src/app/modules/public/pages/room/room.component.ts
+++ src/app/modules/public/pages/room/room.component.ts
@@ -9,5 +9,53 @@ import { faWifi } from "@fortawesome/free-solid-svg-icons";
 export class RoomComponent {
 	faWifi = faWifi;
 
-	roomSpecification = [];
+	responsiveOptions: any = [];
+
+	apartments: any = [
+		{
+			id: 1,
+			name: "Apartment 1",
+			price: 100,
+		},
+		{
+			id: 2,
+			name: "Apartment 2",
+			price: 150,
+		},
+		{
+			id: 3,
+			name: "Apartment 3",
+			price: 150,
+		},
+		{
+			id: 4,
+			name: "Apartment 4",
+			price: 150,
+		},
+		{
+			id: 5,
+			name: "Apartment 5",
+			price: 150,
+		},
+	];
+
+	constructor() {
+		this.responsiveOptions = [
+			{
+				breakpoint: "1024px",
+				numVisible: 3,
+				numScroll: 3,
+			},
+			{
+				breakpoint: "768px",
+				numVisible: 2,
+				numScroll: 2,
+			},
+			{
+				breakpoint: "560px",
+				numVisible: 1,
+				numScroll: 1,
+			},
+		];
+	}
 }
