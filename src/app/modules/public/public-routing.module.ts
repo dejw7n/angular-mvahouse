bc76ff1978458fd9907759ee3183414d5b209eec
dejw7n <dejw7n@gmail.com>
2023-01-18 11:43:40 +0100

    Add book component

--- src/app/modules/public/public-routing.module.ts
+++ src/app/modules/public/public-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { BookComponent } from "./pages/book/book.component";
 import { HomeComponent } from "./pages/home/home.component";
 import { RoomComponent } from "./pages/room/room.component";
 import { SearchResultsComponent } from "./pages/search-results/search-results.component";
@@ -8,6 +9,7 @@ const routes: Routes = [
 	{ path: "", component: HomeComponent },
 	{ path: "searchresults", component: SearchResultsComponent },
 	{ path: "room", component: RoomComponent },
+	{ path: "book", component: BookComponent },
 ];
 
 @NgModule({
